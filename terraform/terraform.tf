3ccfd2e48c02a8623ed1cc6a81f33845fac3e40b
amitranjan0220 <31431972+amitranjan0220@users.noreply.github.com>
2024-07-22 23:48:51 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -11,6 +11,6 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  access_key = ${AWS_ACCESS_KEY_ID}
-  secret_key = ${AWS_SECRET_ACCESS_KEY}
+  access_key = ${{ AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ AWS_SECRET_ACCESS_KEY }}
 }
