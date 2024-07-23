64a929ce4f0afcaa7123ac46bbbe437bb9cfd422
Amit Ranjan <aranjan@ex2india.com>
2024-07-23 12:07:25 +0530

    updated files

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.59.0"
     }
   }
+  #backend "s3" {
+  #  bucket         = "geocoding-terraform-state"
+  #  key            = "state/terraform.tfstate"
+  #  region         = "ap-south-1"
+  # dynamodb_table = "terraform-state"
+  # encrypt        = true
+  #}
 
   required_version = "~> 1.2"
 }
